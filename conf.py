fa96f856dc9889db7895c8110c0d27b406fa3fc7
Danya <danillewin@gmail.com>
2021-05-27 17:44:40 +0300

    Sticky sidebar: moved css rules to proper CSS file

--- conf.py
+++ conf.py
@@ -132,7 +132,6 @@ html_title = 'Hangfire Documentation'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['css/hangfire.css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
