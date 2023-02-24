8f6e25a8fd0fb0a849ac532b3f32e6a4bbabc38c
Sergey Odinokov <odinserj@gmail.com>
2023-02-24 15:44:20 +0700

    Update documentation_options.js

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -4,7 +4,9 @@ var DOCUMENTATION_OPTIONS = {
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true
 };
